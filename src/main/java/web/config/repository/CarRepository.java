b562ccea40138d058868e3b84e366b63bf1e6e1f
Antonmak112 <pyanton112@gmail.com>
2023-08-16 14:59:02 +0500

    fixed

---
+++ src/main/java/web/config/repository/CarRepository.java
@@ -0,0 +1,25 @@
+package web.config.repository;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CarRepository {
+    private static final List<Car> carList = new ArrayList<>(8);
+
+    public CarRepository() {
+        carList.add(new Car("BMW", "5M", 15000.0));
+        carList.add(new Car("LADA", "PRIORA", 3000.0));
+        carList.add(new Car("AUDI", "Q5", 12000.0));
+        carList.add(new Car("TOYOTA", "CROWN", 9000.0));
+        carList.add(new Car("BMW", "3", 11000.0));
+    }
+
+    public List<Car> getAllCars() {
+        return carList;
+    }
+
+}
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,24 +1,23 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.config.repository.CarRepository;
 import web.model.Car;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    CarRepository carRepository;
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(name = "count", defaultValue = "5") int count, Model model){
-        List<Car> listCars = new ArrayList<>(10);
-        listCars.add(new Car("BMW","5M",15000.0));
-        listCars.add(new Car("LADA","PRIORA",3000.0));
-        listCars.add(new Car("AUDI","Q5",12000.0));
-        listCars.add(new Car("TOYOTA","CROWN",9000.0));
-        listCars.add(new Car("BMW","3",11000.0));
+        List<Car> listCars = carRepository.getAllCars();
         List<Car> carsToShow;
 
         if (count >= 5) {
